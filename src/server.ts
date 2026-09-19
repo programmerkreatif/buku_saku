bc8c1cd66fd8b352ec676b3d193b3f969f23e7cc
Programmer Kreatif <programmerkreatif@gmail.com>
2026-09-19 21:04:43 +0700

    fix server ts

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import express from "express";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 9000;
 
 app.use(express.json());
 
-app.get("api/hello", (req, res) => {  
+app.get("/api/hello", (req, res) => {  
     res.json({
     	success:true,
 	message:"Hello, world nodejs exploration"
@@ -17,3 +17,6 @@ app.get("api/hello", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Listening port:${PORT}`);
 });
+
+
+export default app;

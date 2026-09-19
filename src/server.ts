02ba1dc5e8c346b46d2cbe4fce4c7c4c574966b1
Programmer Kreatif <programmerkreatif@gmail.com>
2026-09-19 19:20:52 +0700

    add basic configuration

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("api/hello", (req, res) => {  
+    res.json({
+    	success:true,
+	message:"Hello, world nodejs exploration"
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listening port:${PORT}`);
+});
